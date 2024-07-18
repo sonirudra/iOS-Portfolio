29bf9b15f5c2a1f21338e9d3fa1cffdf57e2daa6
soniRudra <rudrasoni6080@gmail.com>
2024-07-18 19:00:40 +0530

    new cheque split app added

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by DREAMWORLD on 18/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
+    @State private var chequeAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
+    
+    private var finalSplitAmount: Double {
+        let totalNumberOfPeople = Double(numberOfPeople + 2)
+        let selectedTipPercentage = Double(tipPercentage)
+        
+        let tipAmount = chequeAmount / 100 * selectedTipPercentage
+        let totalWithTip = chequeAmount + tipAmount
+        let grandTotal = chequeAmount + totalWithTip
+        let perPersonAmount = grandTotal / totalNumberOfPeople
+        
+        return perPersonAmount
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) { number in
+                            Text("\(number) people")
+                        }
+                    }
+                }
+                
+                Section("Tip amount") {
+                    Picker("Number of people", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { percentage in
+                            Text(percentage, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Each person has to pay") {
+                    Text(finalSplitAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by DREAMWORLD on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
